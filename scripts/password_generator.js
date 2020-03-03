5846d7e26873bdee4cb79009a682593ee7009390
Mariusz Boruc <mariuszbrc@gmail.com>
2020-03-03 22:24:41 +1030

    Adding bootstrap and css to javascript.

--- scripts/password_generator.js
+++ scripts/password_generator.js
@@ -1,22 +1,13 @@
-// Password Generator Script
-// Requires classes.js where class definitions are stored
+// Hide some elements of the website
+document.getElementById("btn_generate").style.display = "hide";
 
-let user_length=prompt("Provide password length:");
-let user_lower=confirm("Do you want to include lower characters?:");
-let user_upper=confirm("Do you want to include upper characters?:");
-let user_numerical=confirm("Do you want to include numerical characters?:");
-let user_special=confirm("Do you want to include special characters?:");
+// Once Start button is clicked, unhide other elements
+document.getElementById("btn_start").onclick  = function () {
 
-// Create initial dictionary
-let dictionary = new classDictionary(user_lower,user_upper,user_numerical,user_special);
-// Shuffle characters within the dictionary
-random_dictionary = dictionary.shuffle_dictionary();
+    console.log("WHAT");
+    document.getElementById("btn_generate").style.display = "block";
+  
+  };
 
 
 
-// Create initial empty password
-let password = new classPassword(user_length,user_lower,user_upper,user_numerical,user_special);
- 
-// Generate and print out password
-password.generate_password(random_dictionary);
-password.print_2_console();
