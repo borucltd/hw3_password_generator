7143f61209c3251bcf5d8330a03f148607c36d7a
Mariusz Boruc <mariuszbrc@gmail.com>
2020-03-03 22:34:36 +1030

    Can hide elements now.

--- scripts/password_generator.js
+++ scripts/password_generator.js
@@ -1,10 +1,16 @@
 // Hide some elements of the website
-document.getElementById("btn_generate").style.display = "hide";
+document.getElementById("btn_generate").style.display = "none";
+
+let sections = [ "length","upper","lower","numbers","special"];
+for (var i = 0; i < sections.length; i++){
+
+    document.getElementById("section_"+sections[i]).style.display = "none";
+}
+
 
 // Once Start button is clicked, unhide other elements
 document.getElementById("btn_start").onclick  = function () {
 
-    console.log("WHAT");
     document.getElementById("btn_generate").style.display = "block";
   
   };
