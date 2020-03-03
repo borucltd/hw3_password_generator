228c1085393858e95eae6beaaffbf05edc3d15f1
Mariusz Boruc <mariuszbrc@gmail.com>
2020-03-03 23:08:58 +1030

    Working html/bootstrap with javascript.

--- scripts/classes.js
+++ scripts/classes.js
@@ -128,32 +128,3 @@ class classPassword {
     }
 
 };
-
-
-
-
-
-
-
-// Password Generator Script
-// Requires classes.js where class definitions are stored
-
-let user_length=prompt("Provide password length:");
-let user_lower=confirm("Do you want to include lower characters?:");
-let user_upper=confirm("Do you want to include upper characters?:");
-let user_numerical=confirm("Do you want to include numerical characters?:");
-let user_special=confirm("Do you want to include special characters?:");
-
-// Create initial dictionary
-let dictionary = new classDictionary(user_lower,user_upper,user_numerical,user_special);
-// Shuffle characters within the dictionary
-random_dictionary = dictionary.shuffle_dictionary();
-
-
-
-// Create initial empty password
-let password = new classPassword(user_length,user_lower,user_upper,user_numerical,user_special);
- 
-// Generate and print out password
-password.generate_password(random_dictionary);
-password.print_2_console();
--- scripts/password_generator.js
+++ scripts/password_generator.js
@@ -1,7 +1,10 @@
 // Hide some elements of the website
+// Array of sections
+let sections = [ "length","upper","lower","numbers","special","progressbar"];
+
 document.getElementById("btn_generate").style.display = "none";
 
-let sections = [ "length","upper","lower","numbers","special"];
+
 for (var i = 0; i < sections.length; i++){
 
     document.getElementById("section_"+sections[i]).style.display = "none";
@@ -9,11 +12,43 @@ for (var i = 0; i < sections.length; i++){
 
 
 // Once Start button is clicked, unhide other elements
+// and initiate variables
 document.getElementById("btn_start").onclick  = function () {
 
     document.getElementById("btn_generate").style.display = "block";
-  
-  };
 
+    for (var i = 0; i < sections.length; i++){
+
+        document.getElementById("section_"+sections[i]).style.display = "block";
+    }
+
+    
+};
+
+
+
+
+// Once Generate button is clicked, do what it is suppose to do
+document.getElementById("btn_generate").onclick  = function () {
+
+    let user_length=document.getElementById("password_length").value;
+    let user_lower=document.getElementById("check_lower").value;
+    let user_upper=document.getElementById("check_upper").value;
+    let user_numerical=document.getElementById("check_numbers").value;
+    let user_special=document.getElementById("check_special").value;
+
+    // Create initial dictionary
+    let dictionary = new classDictionary(user_lower,user_upper,user_numerical,user_special);
+    // Shuffle characters within the dictionary
+    random_dictionary = dictionary.shuffle_dictionary();
+
+    // Create initial empty password
+    let password = new classPassword(user_length,user_lower,user_upper,user_numerical,user_special);
+    
+    // Generate and print out password
+    password.generate_password(random_dictionary);
+    password.print_2_console();
+
+};
 
 
